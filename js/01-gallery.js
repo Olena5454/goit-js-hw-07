d3738dca93631bc431d5975d772d25babbac4c30
Olena5454 <sinicki2018@gmail.com>
2023-01-22 14:08:12 +0200

    only_the_first_task

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,58 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
+
+
 console.log(galleryItems);
+const containerGallery = document.querySelector(".gallery");
+
+function galleryItemsCards(galleryItems) {
+    return galleryItems
+        .map(
+            ({ preview, original, description }) =>
+                `<div class="gallery__item">
+        <a class="gallery__link" href="${original}">
+          <img
+            class="gallery__image"
+            src="${preview}"
+            data-source="${original}"
+            alt="${description}"
+          />
+        </a>
+      </div>`
+        )
+        .join("");
+
+}
+
+containerGallery.insertAdjacentHTML(
+    "beforeend",
+    galleryItemsCards(galleryItems)
+);
+
+containerGallery.addEventListener("click", onClickItemsCards);
+
+function onClickItemsCards(e) {
+    if (e.target.nodeName !== "IMG") {
+        return;
+    }
+    e.preventDefault();
+
+    const instance = basicLightbox.create(`
+          <img src="${e.target.dataset.source}" width="800" height="600">
+   `);
+
+    instance.show();
+    window.addEventListener("keydown", onEscapePress);
+    function onEscapePress(e) {
+        if (e.code === "Escape") {
+            window.removeEventListener("keydown", onEscapePress);
+            instance.close();
+        }
+    }
+};
+
+
+
+
+
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,28 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
-
 console.log(galleryItems);
+console.log(galleryItems);
+const containerGallery = document.querySelector(".gallery");
+
+function galleryItemsCards(galleryItems) {
+  return galleryItems
+    .map(
+      ({ preview, original, description }) =>
+        `<div class="gallery__item">
+        <a class="gallery__item" href="${original}">
+          <img
+            class="gallery__image"
+            src="${preview}"
+            data-source="${original}"
+            alt="${description}"
+          />
+        </a>
+      </div>`
+    )
+    .join("");
+}
+
+containerGallery.insertAdjacentHTML(
+  "beforeend",
+  galleryItemsCards(galleryItems)
+);
